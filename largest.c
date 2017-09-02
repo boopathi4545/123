a525c8251a96c00ecc2cf0ab404016047d072d3a
boopathi4545 <31505777+boopathi4545@users.noreply.github.com>
2017-09-01 19:50:16 -0700

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("%d enter the different therr number");
+scanf("%d",&a,&b,&c);
+if(a>=b && a>=c)
+printf("%d the largest number is",a);
+elesif(b>=a && b>=c)
+printf("%d the largest number is",b);
+eles
+printf("%d the largest number is",c);
+}
